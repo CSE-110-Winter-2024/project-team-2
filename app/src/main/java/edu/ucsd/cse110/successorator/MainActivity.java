a6e1c502fad227b553010182757a3495434ab8da
andvhuynh <andyhuvnh@gmail.com>
2024-02-04 15:09:17 -0800

    Tasks1-2,1-3

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -16,22 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Sets the default text for initial screen with no goals
         var view = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
         view.placeholderText.setText(R.string.default_text);
 
+        //displays default screen for no goals
         setContentView(view.getRoot());
     }
 
+    //Method to display the add menu item
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.add_bar, menu);
         return true;
     }
 
+    //Method for switching view to add a goal
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         var itemId = item.getItemId();
 
+        //When the add button is clicked, switch view to the enter_goal view
         if (itemId == R.id.add_bar_menu_swap_views){
             setContentView(R.layout.enter_goal);
         }
