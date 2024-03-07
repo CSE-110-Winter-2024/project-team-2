ccde8447c611248ceef734dff8499393b3867b3c
Hannah Coates <hcoates@ucsd.edu>
2024-03-06 17:52:14 -0800

    #104 Testing

--- lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/GoalTest.java
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/GoalTest.java
@@ -24,7 +24,7 @@ public class GoalTest {
     public void getGoal() {
         for (int i = 0; i < 100; i++) {
             String theGoal = "Test goal " + i;
-            Goal goal = new Goal(0, theGoal, 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+            Goal goal = new Goal(0, theGoal, 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
             assertEquals(goal.getGoalText(), theGoal);
         }
     }
@@ -32,7 +32,7 @@ public class GoalTest {
     @Test
     public void getId() {
         for (int i = 0; i < 100; i++) {
-            Goal goal = new Goal(i, "Test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+            Goal goal = new Goal(i, "Test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
             assertEquals(Integer.valueOf(i), goal.getId());
         }
     }
@@ -40,21 +40,21 @@ public class GoalTest {
     @Test
     public void getSortOrder() {
         for (int i = 0; i < 100; i++) {
-            Goal goal = new Goal(0, "Test Goal", i, false, null, true, null, false, GoalContext.getGoalContextById(1));
+            Goal goal = new Goal(0, "Test Goal", i, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
             assertEquals(Integer.valueOf(i), goal.getSortOrder());
         }
     }
 
     @Test
     public void getIsComplete() {
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         assertFalse(goal.getIsComplete());
     }
 
     @Test
     public void getDateCompleted() {
         Calendar dateCompleted = Calendar.getInstance();
-        Goal goal = new Goal(0, "test Goal", 0, false, dateCompleted, true, null, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, dateCompleted, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         dateCompleted.set(Calendar.HOUR, 0);
         dateCompleted.set(Calendar.MINUTE, 0);
         dateCompleted.set(Calendar.SECOND, 0);
@@ -65,15 +65,15 @@ public class GoalTest {
 
     @Test
     public void getIsDisplayed() {
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         assertTrue(goal.getIsDisplayed());
-        Goal goal2 = new Goal(0, "test Goal", 0, false, null, false, null, false, GoalContext.getGoalContextById(1));
+        Goal goal2 = new Goal(0, "test Goal", 0, false, null, false, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         assertFalse(goal2.getIsDisplayed());
     }
 
     @Test
     public void setIsDisplayed() {
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         assertTrue(goal.getIsDisplayed());
 
         goal.setIsDisplayed(false);
@@ -83,23 +83,23 @@ public class GoalTest {
     @Test
     public void getGoalDate() {
         Calendar currDate = Calendar.getInstance();
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, currDate, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, currDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         assertEquals(currDate, goal.getGoalDate());
     }
 
     @Test
     public void getIsPending() {
         Calendar currDate = Calendar.getInstance();
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, currDate, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, currDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         assertFalse(goal.getIsPending());
-        Goal goal2 = new Goal(0, "test Goal", 0, false, null, true, currDate, true, GoalContext.getGoalContextById(1));
+        Goal goal2 = new Goal(0, "test Goal", 0, false, null, true, currDate, true, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         assertTrue(goal2.getIsPending());
     }
 
     @Test
     public void setGoalDate() {
         Calendar currDate = Calendar.getInstance();
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal.setGoalDate(currDate);
         assertEquals(currDate, goal.goalDate);
     }
@@ -107,14 +107,14 @@ public class GoalTest {
     @Test
     public void setIsPending() {
         Calendar currDate = Calendar.getInstance();
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, currDate, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, currDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal.setIsPending(true);
         assertTrue(goal.isPending);
     }
 
     @Test
     public void changeIsCompleteStatus() {
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal.changeIsCompleteStatus();
         assertTrue(goal.isComplete);
     }
@@ -133,48 +133,48 @@ public class GoalTest {
         Boolean isPending = true;
 
         // When not goal is not completed, TODAY view, goal made TODAY
-        Goal goal = new Goal(0, "test Goal", 0, false, null, true, todayGoalDate, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, false, null, true, todayGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal.updateIsDisplayed(currDate, ViewOptions.TODAY);
         assertTrue(goal.getIsDisplayed());
 
         // When not goal is not completed, TODAY view, goal made yesterday
-        goal = new Goal(0, "test Goal", 0, false, null, true, yesterdayGoalDate, false, GoalContext.getGoalContextById(1));
+        goal = new Goal(0, "test Goal", 0, false, null, true, yesterdayGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal.updateIsDisplayed(currDate, ViewOptions.TODAY);
         assertTrue(goal.getIsDisplayed());
 
         // When not goal is not completed, TODAY view, goal made TOMORROW
-        Goal goal1 = new Goal(0, "test Goal", 0, false, null, true, tommorowGoalDate, false, GoalContext.getGoalContextById(1));
+        Goal goal1 = new Goal(0, "test Goal", 0, false, null, true, tommorowGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal1.updateIsDisplayed(currDate, ViewOptions.TODAY);
         assertFalse(goal1.getIsDisplayed());
 
         // When not goal is not completed, TODAY view, goal is PENDING
-        Goal goal2 = new Goal(0, "test Goal", 0, false, null, true, null, isPending, GoalContext.getGoalContextById(1));
+        Goal goal2 = new Goal(0, "test Goal", 0, false, null, true, null, isPending, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal2.updateIsDisplayed(currDate, ViewOptions.TODAY);
         assertFalse(goal2.getIsDisplayed());
 
         // When not goal is not completed, TOMORROW view, goal made TOMORROW
-        Goal goal3 = new Goal(0, "test Goal", 0, false, null, true, tommorowGoalDate, false, GoalContext.getGoalContextById(1));
+        Goal goal3 = new Goal(0, "test Goal", 0, false, null, true, tommorowGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal3.updateIsDisplayed(tomorrowDate, ViewOptions.TOMORROW);
         assertTrue(goal3.getIsDisplayed());
 
         // When not goal is not completed, TOMORROW view, goal made TODAY
-        goal3 = new Goal(0, "test Goal", 0, false, null, true, todayGoalDate, false, GoalContext.getGoalContextById(1));
+        goal3 = new Goal(0, "test Goal", 0, false, null, true, todayGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal3.updateIsDisplayed(tomorrowDate, ViewOptions.TOMORROW);
         assertFalse(goal3.getIsDisplayed());
 
         // When not goal is not completed, TOMORROW view, goal made yesterday
-        goal3 = new Goal(0, "test Goal", 0, false, null, true, yesterdayGoalDate, false, GoalContext.getGoalContextById(1));
+        goal3 = new Goal(0, "test Goal", 0, false, null, true, yesterdayGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal3.updateIsDisplayed(tomorrowDate, ViewOptions.TOMORROW);
         assertFalse(goal3.getIsDisplayed());
 
         // Today list: Same day goal is crossed off
-        Goal goal4 = new Goal(0, "test Goal", 0, true, dateCompleted, true, todayGoalDate, false, GoalContext.getGoalContextById(1));
+        Goal goal4 = new Goal(0, "test Goal", 0, true, dateCompleted, true, todayGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal4.updateIsDisplayed(currDate, ViewOptions.TODAY);
         assertTrue(goal4.getIsDisplayed());
 
         // Today list: Day after goal is crossed off
         currDate.add(Calendar.DATE, 1);
-        Goal goal5 = new Goal(0, "test Goal", 0, true, dateCompleted, true, todayGoalDate, false, GoalContext.getGoalContextById(1));
+        Goal goal5 = new Goal(0, "test Goal", 0, true, dateCompleted, true, todayGoalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal5.updateIsDisplayed(currDate, ViewOptions.TODAY);
         assertFalse(goal5.getIsDisplayed());
     }
@@ -198,7 +198,7 @@ public class GoalTest {
         Calendar currDate = Calendar.getInstance();
         currDate.setTime(sdf.parse("Feb 17 01:59:00 AM 2024"));
         Calendar goalDate = (Calendar) currDate.clone();
-        Goal goal = new Goal(0, "test Goal", 0, true, dateCompleted, true, goalDate, false, GoalContext.getGoalContextById(1));
+        Goal goal = new Goal(0, "test Goal", 0, true, dateCompleted, true, goalDate, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
         goal.updateIsDisplayed(currDate, ViewOptions.TODAY);
         assertTrue(goal.getIsDisplayed());
 
@@ -225,7 +225,7 @@ public class GoalTest {
     @Test
     public void withSortOrder() {
         for (int i = 0; i < 100; i++) {
-            Goal goal = new Goal(0, "Test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+            Goal goal = new Goal(0, "Test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
             Goal actual = goal.withSortOrder(i);
             assertEquals(Integer.valueOf(i), actual.getSortOrder());
         }
@@ -234,7 +234,7 @@ public class GoalTest {
     @Test
     public void withId() {
         for (int i = 0; i < 100; i++) {
-            Goal goal = new Goal(0, "Test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1));
+            Goal goal = new Goal(0, "Test Goal", 0, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
             Goal actual = goal.withId(i);
             assertEquals(Integer.valueOf(i), actual.getId());
         }
@@ -243,9 +243,9 @@ public class GoalTest {
     @Test
     public void testEquals() {
         for (int i = 0; i < 100; i++) {
-            Goal goal1 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1));
-            Goal goal2 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1));
-            Goal goal3 = new Goal(i, "Test Goal", i, false, null, true, null, false, GoalContext.getGoalContextById(1));
+            Goal goal1 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
+            Goal goal2 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
+            Goal goal3 = new Goal(i, "Test Goal", i, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
             assertEquals(goal1, goal2);
             assertNotEquals(goal1, goal3);
         }
@@ -254,9 +254,9 @@ public class GoalTest {
     @Test
     public void testHashCode() {
         for (int i = 0; i < 100; i++) {
-            Goal goal1 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1));
-            Goal goal2 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1));
-            Goal goal3 = new Goal(i, "Test Goal", i, false, null, true, null, false, GoalContext.getGoalContextById(1));
+            Goal goal1 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
+            Goal goal2 = new Goal(i, "Test Goal", i+5, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
+            Goal goal3 = new Goal(i, "Test Goal", i, false, null, true, null, false, GoalContext.getGoalContextById(1), 0, Goal.RecurrencePattern.NONE, null, null);
             assertEquals(goal1.hashCode(), goal2.hashCode());
             assertNotEquals(goal1.hashCode(), goal3.hashCode());
         }
@@ -265,10 +265,12 @@ public class GoalTest {
     @Test
     public void getGoalContextId() {
         Goal goal = new Goal(0, "test Goal", 0, false, null,
-                true, null, false, GoalContext.getGoalContextById(1));
+                true, null, false, GoalContext.getGoalContextById(1),
+                0, Goal.RecurrencePattern.NONE, null, null);
         assertEquals(goal.getGoalContext(), GoalContext.getGoalContextById(1));
         Goal goal2 = new Goal(0, "test Goal", 0, false, null,
-                false, null, false, GoalContext.getGoalContextById(4));
+                false, null, false, GoalContext.getGoalContextById(4),
+                0, Goal.RecurrencePattern.NONE, null, null);
         assertEquals(goal2.getGoalContext(), GoalContext.getGoalContextById(4));
     }
 }
